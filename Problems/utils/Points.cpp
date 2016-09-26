ac9bb6968e7ee1e4e0356a571371b0b5415c7a74
skull0801 <marcus.080196@gmail.com>
2016-09-25 21:26:19 -0300

    adding some util files (Points)

---
+++ Problems/utils/Points.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+
+#define PI 3.141592653589793
+
+class Point {
+public:
+    double x, y;
+
+    Point(double xv = 0, double yv = 0) : x(xv), y(yv) {}
+}
+
+double distance(const Point& P, const Point& Q)
+{
+    return hypot(P.x - Q.x, P.y - Q.y);
+}
